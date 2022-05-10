2836ca3ba86116742cb50befb128f34587d3c531
rzddkh <88165294+rzddkh@users.noreply.github.com>
2022-05-10 16:48:09 -0700

    endpoint is created for the images to be processesd and stored the process image in thumb folder

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import express from 'express';
-import pug from './routes/api/resize';
-import { imageProcess } from './routes/api/resize';
-import url from 'url';
-//const url=require('url')
+import imageProcess from './routes/api/resize';
 const app = express();
 const port = 3000;
 
@@ -12,9 +9,11 @@ app.get('/', (req, res) => {
   res.send(`server is connected at port ${port}`);
 });
 
-app.get('/images/',pug, (req,res)=>{
-    const filename = (req.url).split('?')[1].split('&')[0].split('=')[1];
-    res.sendFile(`images/thumb/${filename}.jpg`, { root: '.' });
+
+app.get('/images/', async (req,res)=>{
+    await imageProcess(req.url);
+    const filename = req.query.filename;
+    res.sendFile(`images/thumb/${filename}-thumb.jpg`, { root: '.' });
 });
 
 app.listen(port, () => {
--- src/routes/api/resize.ts
+++ src/routes/api/resize.ts
@@ -1,20 +1,14 @@
 import sharp from "sharp";
-import express from 'express';
-
-const imageProcess = (url : string) => {
-
-    const filename: string = (url).split('?')[1].split('&')[0].split('=')[1];
-    const height: number = + (url).split('?')[1].split('&')[2].split('=')[1];
-    const width: number = + (url).split('?')[1].split('&')[1].split('=')[1];
-    sharp(`./images/full/${filename}.jpg`).resize({width: width, height: height}).toFile(`./images/thumb/${filename}.jpg`);
+const imageProcess = async (url : string) => {
+    const urlParams=new URLSearchParams(url);
+    const filename =(urlParams.get('/images?filename'));
+    const width=(urlParams.get('width') as unknown) as number;
+    const height =(urlParams.get('height') as unknown )as number;  
+    //fs.writeFile(`./images/thumb/${filename}.jpg`,newImage);
+   await sharp(`./images/full/${filename}.jpg`).resize({ width: +width, height: +height }).toFile(`./images/thumb/${filename}-thumb.jpg`);
 }
 
-const pug = (req : express.Request, res : express.Response, next : Function) : void => {
-    imageProcess(req.url);
-    next();
-}
 
-export default pug;
-export {
-    imageProcess
-};
+
+export default imageProcess;
+
