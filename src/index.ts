4a136bf5900521359fc936de1f4477930252ea9f
rzddkh <88165294+rzddkh@users.noreply.github.com>
2022-05-11 12:47:01 -0700

    cashing feature added + the endpoint moved to its own module from index.ts to resize.ts in api folder

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
 import express from 'express';
-import imageProcess from './routes/api/resize';
+import resize from './routes/api/resize';
+
 const app = express();
 const port = 3000;
 
-
-// sample url : http://localhost:3000/images?filename=pug&width=1000&height=250
 app.get('/', (req, res) => {
   res.send(`server is connected at port ${port}`);
 });
 
-
-app.get('/images/', async (req,res)=>{
-    await imageProcess(req.url);
-    const filename = req.query.filename;
-    res.sendFile(`images/thumb/${filename}-thumb.jpg`, { root: '.' });
-});
+// end point for resizing an image
+app.use('/', resize);
 
 app.listen(port, () => {
   console.log('server is live!');
 });
-
--- src/routes/api/resize.ts
+++ src/routes/api/resize.ts
@@ -1,14 +1,28 @@
-import sharp from "sharp";
-const imageProcess = async (url : string) => {
-    const urlParams=new URLSearchParams(url);
-    const filename =(urlParams.get('/images?filename'));
-    const width=(urlParams.get('width') as unknown) as number;
-    const height =(urlParams.get('height') as unknown )as number;  
-    //fs.writeFile(`./images/thumb/${filename}.jpg`,newImage);
-   await sharp(`./images/full/${filename}.jpg`).resize({ width: +width, height: +height }).toFile(`./images/thumb/${filename}-thumb.jpg`);
-}
+import sharp from 'sharp';
+import express from 'express';
+import { access } from 'node:fs/promises';
 
+const resize = express.Router();
 
+resize.get('/images/resize', async (req, res) => {
+  const filename: string = req.query.filename as unknown as string;
+  const width: string = req.query.width as unknown as string;
+  const height: string = req.query.height as unknown as string;
 
-export default imageProcess;
+  //check to see if the file exists if so we just send the file
+  //if not we resize the image and create the new image and then we send it
+  try {
+    await access(`./images/thumb/${filename}-${width}-${height}.jpg`);
+    console.log('image already exists!');
+  } catch {
+    console.error('image will be resized!');
+    await sharp(`./images/full/${filename}.jpg`)
+      .resize({ width: +width, height: +height })
+      .toFile(`./images/thumb/${filename}-${width}-${height}.jpg`);
+  }
+  res.sendFile(`./images/thumb/${filename}-${width}-${height}.jpg`, {
+    root: '.'
+  });
+});
 
+export default resize;
